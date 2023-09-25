5b9ca98c4b14e9e888abf9e389b623e525896633
stu96534 <stu96534@gmail.com>
2023-09-25 21:54:48 +0800

    feat: delete defult view

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="'navbar navbar-expand-lg navbar-light'">
+    <div class="container-md">
+      <h1>貨幣轉換功能</h1>
+    </div>
+  </nav>
+</template>
+
+<style>
+nav {
+  border-bottom: 0.8px;
+  border-color: white;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
+import "bootstrap"
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
