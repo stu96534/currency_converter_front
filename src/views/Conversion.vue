ea0da9c826f2f7cae6b4aa905917a7da9292ce18
stu96534 <stu96534@gmail.com>
2023-10-24 15:44:17 +0800

    feat: made nation button

--- src/views/Conversion.vue
+++ src/views/Conversion.vue
@@ -2,9 +2,61 @@
 </script>
 
 <template>
-  <div class="not-found">
-    <h1>hallo</h1>
+  <div class="container">
+    <div class="nation">
+  <button class="card">
+    <img src="../../public/usa-flag.jpg" alt="">
+    <h3>美金(USD)</h3>
+  </button>
+  <button class="card">
+      <img src="../../public/usa-flag.jpg" alt="">
+      <h3>美金(USD)</h3>
+    </button>
+    <button class="card">
+      <img src="../../public/usa-flag.jpg" alt="">
+      <h3>美金(USD)</h3>
+    </button>
+    <button class="card">
+      <img src="../../public/usa-flag.jpg" alt="">
+      <h3>美金(USD)</h3>
+    </button>
+    
+    </div>
+    
   </div>
 </template>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+}
+
+.container{
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.nation{
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  grid-template-rows: auto auto auto auto;
+  grid-gap: 20px;
+}
+
+img {
+  width: 80px;
+}
+
+.card {
+  background: white;
+  border-radius: 12px 12PX;
+  box-shadow: 0 2px 12px rgba(32, 32, 32, .3);
+  width: 85px;
+  height: 85px;
+}
+
+</style>
